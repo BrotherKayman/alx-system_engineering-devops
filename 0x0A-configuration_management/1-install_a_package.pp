6e62081759eab8aa79d0cc2e3c5e58e04f25647d
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2024-02-29 18:10:23 +0200

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/pup
-# Install a specific version of flask (2.1.0)
-package {'flask':
+# Install a specific version of Flask (2.1.0) using pip3
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
