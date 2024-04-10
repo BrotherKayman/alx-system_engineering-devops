cdf90c138e350a018037052f94620379fe5cbd68
Kagiso (BrotherKayman) Motlhaoleng <135019661+BrotherKayman@users.noreply.github.com>
2024-04-10 10:59:14 +0200

    Update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""Queries the Reddit API and prints titles of the first 10"""
+"""Contains top_ten function"""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Prints titles of the 10 hot posts listed on a given subreddit.
-    """
+    """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "0x16-api_advanced:project:\
-        v1.0.0 (by /u/firdaus_cartoon_jr)"
+v1.0.0 (by /u/firdaus_cartoon_jr)"
     }
     params = {
         "limit": 10
